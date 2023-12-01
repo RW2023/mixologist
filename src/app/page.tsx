eb84d019d68e0d38a5f4a3fb67c25bd9127acc7d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-01 12:31:05 -0500

    edit home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,20 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='flex flex-col flex-wrap items-center'>
-      <h1 className='text-3xl'>Home Page</h1>
+    <div className='flex flex-col flex-wrap items-center h-screen justify-center align-middle'>
+      <h1 className='text-3xl m-2'>Home Page</h1>
       <Image
         src="/images/cocktail.png"
         alt="cocktail"
         width={500}
         height={500}
-        className='rounded-md border-2 border-gray-500'
+        className='rounded-md border-2 border-base-500 bg-base-500 p-2'
       />
+      <div>
+        <p className='text-2xl text-center'>
+         Welcome to the Mixologist App
+        </p>
+      </div>
     </div>
   )
 }

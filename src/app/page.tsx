63a146865fb07e13ce5367ce337ff2828a3dcb82
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-01 18:26:05 -0500

    update image dimension and pass prop to subheading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
-import Image from 'next/image'
+import Heading from '@/Components/ui/Heading';
+import SubHeading from '@/Components/ui/SubHeading';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className='flex flex-col flex-wrap items-center h-screen justify-center align-middle'>
-      <h1 className='text-3xl m-2'>Home Page</h1>
-      <Image
-        src="/images/cocktail.png"
-        alt="cocktail"
-        width={500}
-        height={500}
-        className='rounded-md border-2 border-base-500 bg-base-500 p-2'
-      />
-      <div>
-        <p className='text-2xl text-center'>
-         Welcome to the Mixologist App
-        </p>
+    <div className="flex flex-col flex-wrap h-screen justify-center text-3xl items-center">
+      <div className="text text-4xl">
+        <Heading title="Mixologist" iconClass="fas fa-cocktail" />
+        <SubHeading
+          title="Your one stop shop for all things cocktails"
+          iconClass="fas fa-brain"
+        />
+        <Image
+          src="/images/cocktail.png"
+          alt="cocktail"
+          width={400}
+          height={400}
+          className="rounded-lg border-2 border-base-500 bg-base-300 p-2 items-center justify-center mx-auto mb-4"
+        />
       </div>
+
+      <SubHeading title='search for your favorite cocktail, or find a new one to try! ' iconClass='fas fa-search' />
     </div>
-  )
+  );
 }

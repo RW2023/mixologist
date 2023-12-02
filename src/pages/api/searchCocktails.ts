011189edbc3a78f24f777bae563b74e3df5540ae
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-02 06:36:19 -0500

    create cocktail search endpoint

---
+++ src/pages/api/searchCocktails.ts
@@ -0,0 +1,38 @@
+// pages/api/searchCocktails.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  const ingredient = req.query.ingredient;
+
+  if (!ingredient || typeof ingredient !== 'string') {
+    return res
+      .status(400)
+      .json({
+        error: 'Ingredient query parameter is required and must be a string',
+      });
+  }
+
+  try {
+    const options = {
+      method: 'GET',
+      url: 'https://the-cocktail-db.p.rapidapi.com/filter.php',
+      params: { i: ingredient },
+      headers: {
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com',
+      },
+    };
+
+    const response = await axios.request(options);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .json({ error: 'Error fetching data from The Cocktail Database' });
+  }
+}

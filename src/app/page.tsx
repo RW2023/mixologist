91527edfef5340c2161696e6b71d8979edab9050
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-12-01 19:45:34 -0500

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,30 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-wrap h-screen justify-center text-3xl items-center">
-      <div className="text text-4xl">
-        <Heading title="Mixologist" iconClass="fas fa-cocktail" />
-        <SubHeading
-          title="Your one stop shop for all things cocktails"
-          iconClass="fas fa-brain"
-        />
-        <Image
-          src="/images/cocktail.png"
-          alt="cocktail"
-          width={400}
-          height={400}
-          className="rounded-lg border-2 border-base-500 bg-base-300 p-2 items-center justify-center mx-auto mb-4"
-        />
+    <div className="flex flex-col items-center justify-center h-screen container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="text-center">
+        <Heading title="cocktail professor" iconClass="fas fa-cocktail" />
+        <div className="inline-block max-w-xs mx-auto sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl">
+          <Image
+            src="/images/cocktail.png"
+            alt="cocktail"
+            width={400}
+            height={400}
+            className="rounded-lg border-2 border-base-500 bg-base-300 p-2"
+          />
+        </div>
       </div>
 
-      <SubHeading title='search for your favorite cocktail, or find a new one to try! ' iconClass='fas fa-search' />
+      <SubHeading
+        title="search for your favorite cocktail!"
+        iconClass="fas fa-search"
+      />
+      <button
+        type="button"
+        className="btn rounded-lg text-3xl fas fa-martini-glass"
+      >
+        Enter
+      </button>
     </div>
   );
 }

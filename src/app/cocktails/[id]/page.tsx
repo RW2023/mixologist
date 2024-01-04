9ba7c1366f8e9008f9608f2e0dd8610e5d836ef9
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-04 03:16:23 -0500

    Refactor cocktail details page layout

--- src/app/cocktails/[id]/page.tsx
+++ src/app/cocktails/[id]/page.tsx
@@ -1,8 +1,8 @@
-//src/
 'use client';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Image from 'next/image';
+import Loading from '@/Components/ui/Loading';
 
 // Define the interface for a Cocktail object
 interface Cocktail {
@@ -13,7 +13,11 @@ interface Cocktail {
   // Include other properties as needed
 }
 
-export default function CocktailDetailsPage({ params }: { params: { id: string } }) {
+export default function CocktailDetailsPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const [cocktailDetails, setCocktailDetails] = useState<Cocktail | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
@@ -37,23 +41,31 @@ export default function CocktailDetailsPage({ params }: { params: { id: string }
     }
   }, [params.id]);
 
-  if (isLoading) return <div>Loading...</div>;
+  if (isLoading) return <Loading />;
   if (error) return <div>Error: {error}</div>;
-  if (!cocktailDetails) return <div>No cocktail details available</div>;
+  if (!cocktailDetails) return <div className='min-h-screen'>Details Pending...</div>;
 
   return (
-    <div>
-      <h1>{cocktailDetails.strDrink}</h1>
-      <Image
-        src={cocktailDetails.strDrinkThumb}
-        alt={cocktailDetails.strDrink}
-        width={500} // Specify the width
-        height={500} // Specify the height
-        layout="responsive"
-      />
-
-      <p>{cocktailDetails.strInstructions}</p>
-      {/* Display other details as needed */}
+    <div className="container mx-auto p-4 md:w-3/4 lg:w-4/5 min-h-screen">
+      <div className="card-compact bg-base-100 shadow-xl">
+        <figure className="p-4">
+          <Image
+            src={cocktailDetails.strDrinkThumb}
+            alt={cocktailDetails.strDrink}
+            width={500}
+            height={500}
+            layout="responsive"
+            className="rounded-lg"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl font-bold">
+            {cocktailDetails.strDrink}
+          </h2>
+          <p className="text-lg">{cocktailDetails.strInstructions}</p>
+          {/* Display other details as needed */}
+        </div>
+      </div>
     </div>
   );
 }

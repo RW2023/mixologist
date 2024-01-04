b22a94181a98dc34df42c0f0f6ba9ff524cd5957
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-04 02:19:26 -0500

    Update loading message in Loading component

--- src/Components/ui/Loading.tsx
+++ src/Components/ui/Loading.tsx
@@ -7,10 +7,9 @@ const Loading: FC = (): JSX.Element => {
       <div className="text-center">
         <p className="text-3xl">
           <span className="m-2">
-            <span className="loading loading-bars loading-lg"></span>
+            <span className="loading loading-bars loading-lg min-"></span>
           </span>{' '}
-          Loading articles.....☝🏾its taking a bit if you can read this message.
-          Our Bad ☹️. any second now...⏱️
+          Loading Drinks.....☝🏾 any second now...⏱️
         </p>
       </div>
     </div>

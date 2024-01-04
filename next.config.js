d90bb181058b94d76e307b4cb6b84eff4c7708dc
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-04 00:56:57 -0500

    Add image domain configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['www.thecocktaildb.com'],
+  },
+  // ... other configurations
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

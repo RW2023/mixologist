443095c42d802e48443b9731c3655e7484f9ef1d
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-04 21:36:44 -0500

    Refactor CocktailDetailsPage component to render dynamic ingredients

--- src/app/cocktails/[id]/page.tsx
+++ src/app/cocktails/[id]/page.tsx
@@ -7,7 +7,6 @@ import Pending from '@/Components/ui/Pending';
 import Heading from '@/Components/ui/Heading';
 import SubHeading from '@/Components/ui/SubHeading';
 
-
 // Define the interface for a Cocktail object
 interface Cocktail {
   idDrink: string;
@@ -17,9 +16,8 @@ interface Cocktail {
   strCategory: string;
   strAlcoholic: string;
   strGlass: string;
-  // Include other properties as needed
+  [key: `strIngredient${number}`]: string | null; // Dynamic ingredient properties
 }
-
 export default function CocktailDetailsPage({
   params,
 }: {
@@ -48,18 +46,34 @@ export default function CocktailDetailsPage({
     }
   }, [params.id]);
 
+  const renderIngredients = (cocktail: Cocktail) => {
+    let ingredients = [];
+    for (let i = 1; i <= 15; i++) {
+      const ingredient = cocktail[`strIngredient${i}`];
+      if (ingredient) {
+        ingredients.push(
+          <p key={i}>
+            <i className="fas fa-lemon mr-2"></i>
+            {ingredient}
+          </p>,
+        );
+      }
+    }
+    return ingredients;
+  }; 
+
   if (isLoading) return <Loading />;
   if (error) return <div>Error: {error}</div>;
   if (!cocktailDetails) return <Pending />;
 
   return (
     <div className="container mx-auto p-4 md:w-3/4 lg:w-4/5 min-h-screen">
-      <Heading title={cocktailDetails.strDrink} iconClass="fas fa-cocktail" />
+      <Heading title={cocktailDetails?.strDrink} iconClass="fas fa-cocktail" />
       <div className="card bg-base-100 shadow-xl">
         <figure className="p-4">
           <Image
-            src={cocktailDetails.strDrinkThumb}
-            alt={cocktailDetails.strDrink}
+            src={cocktailDetails?.strDrinkThumb}
+            alt={cocktailDetails?.strDrink}
             width={500}
             height={500}
             layout="responsive"
@@ -68,26 +82,31 @@ export default function CocktailDetailsPage({
         </figure>
         <div className="card-body bg-base-300 border rounded-md drop-shadow-2xl">
           <SubHeading
-            title={cocktailDetails.strDrink}
+            title={cocktailDetails?.strDrink}
             iconClass="fas fa-cocktail"
           />
           <p>
             <i className="fas fa-list mr-2"></i>
-            <strong>Category:</strong> {cocktailDetails.strCategory}
+            <strong>Category:</strong> {cocktailDetails?.strCategory}
           </p>
           <p>
             <i className="fas fa-wine-glass-alt mr-2"></i>
-            <strong>Type:</strong> {cocktailDetails.strAlcoholic}
+            <strong>Type:</strong> {cocktailDetails?.strAlcoholic}
           </p>
           <p>
             <i className="fas fa-glass-martini-alt mr-2"></i>
-            <strong>Glass:</strong> {cocktailDetails.strGlass}
+            <strong>Glass:</strong> {cocktailDetails?.strGlass}
           </p>
           <p>
             <i className="fas fa-book mr-2"></i>
-            <strong>Instructions:</strong> {cocktailDetails.strInstructions}
+            <strong>Instructions:</strong> {cocktailDetails?.strInstructions}
           </p>
-          {/* Display other details as needed */}
+          <div>
+            <h3 className="text-lg font-bold mb-2">
+              <i className="fas fa-utensils mr-2"></i>Ingredients
+            </h3>
+            {cocktailDetails && renderIngredients(cocktailDetails)}
+          </div>
         </div>
       </div>
     </div>

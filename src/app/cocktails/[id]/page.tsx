7404c6f792eb6ed7f65b1fdb0fa3c2d9a3ee84a3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-01-19 22:19:34 -0500

    Add food pairings for cocktails using open ai api

--- src/app/cocktails/[id]/page.tsx
+++ src/app/cocktails/[id]/page.tsx
@@ -1,11 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import Image from 'next/image';
 import Loading from '@/Components/ui/Loading';
 import Pending from '@/Components/ui/Pending';
 import Heading from '@/Components/ui/Heading';
 import SubHeading from '@/Components/ui/SubHeading';
+import Link from 'next/link';
 
 // Define the interface for a Cocktail object
 interface Cocktail {
@@ -18,6 +19,7 @@ interface Cocktail {
   strGlass: string;
   [key: `strIngredient${number}`]: string | null; // Dynamic ingredient properties
 }
+
 export default function CocktailDetailsPage({
   params,
 }: {
@@ -26,6 +28,8 @@ export default function CocktailDetailsPage({
   const [cocktailDetails, setCocktailDetails] = useState<Cocktail | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [foodPairings, setFoodPairings] = useState<string[]>([]);
+  const [aiLoading, setAiLoading] = useState<boolean>(false);
 
   useEffect(() => {
     async function fetchCocktailDetails() {
@@ -46,6 +50,44 @@ export default function CocktailDetailsPage({
     }
   }, [params.id]);
 
+  useEffect(() => {
+    const fetchFoodPairings = async () => {
+      if (!cocktailDetails) return;
+
+      setAiLoading(true);
+      try {
+        // Extracting ingredients
+        const ingredients = [];
+        for (let i = 1; i <= 15; i++) {
+          const ingredient = cocktailDetails[`strIngredient${i}`];
+          if (ingredient) {
+            ingredients.push(ingredient);
+          }
+        }
+
+        const response = await fetch('/api/openai', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            message: `Suggest food pairings for the cocktail "${
+              cocktailDetails.strDrink
+            }" with ingredients ${ingredients.join(', ')}`,
+          }),
+        });
+        const data = await response.json();
+        setFoodPairings(data.reply.split(', '));
+      } catch (error) {
+        console.error('Error fetching food pairings:', error);
+      } finally {
+        setAiLoading(false);
+      }
+    };
+
+    fetchFoodPairings();
+  }, [cocktailDetails]);
+
   const renderIngredients = (cocktail: Cocktail) => {
     let ingredients = [];
     for (let i = 1; i <= 15; i++) {
@@ -53,14 +95,28 @@ export default function CocktailDetailsPage({
       if (ingredient) {
         ingredients.push(
           <p key={i}>
-            <i className="fas fa-lemon mr-2"></i>
+            <i className="fas fa-check mr-2"></i>
             {ingredient}
           </p>,
         );
       }
     }
     return ingredients;
-  }; 
+  };
+
+  const renderFoodPairings = () => {
+    if (aiLoading) return <Loading />;
+    return (
+      <div>
+        <h3 className="text-lg font-bold mb-2">Suggested Food Pairings</h3>
+        <ul>
+          {foodPairings.map((pairing, index) => (
+            <li key={index}>{pairing}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
 
   if (isLoading) return <Loading />;
   if (error) return <div>Error: {error}</div>;
@@ -69,15 +125,12 @@ export default function CocktailDetailsPage({
   return (
     <div className="container mx-auto p-4 md:w-3/4 lg:w-4/5 min-h-screen">
       <Heading title={cocktailDetails?.strDrink} iconClass="fas fa-cocktail" />
-      <div className="card bg-base-100 shadow-xl">
+      <div className="card bg-base-300 shadow-xl glass">
         <figure className="p-4">
-          <Image
+          <img
             src={cocktailDetails?.strDrinkThumb}
             alt={cocktailDetails?.strDrink}
-            width={500}
-            height={500}
-            layout="responsive"
-            className="rounded-xl border border-base-300 bg-base-100 p-1"
+            className="w-full h-auto object-cover rounded-xl bg-base-300 p-1 border border-1"
           />
         </figure>
         <div className="card-body bg-base-300 border rounded-md drop-shadow-2xl">
@@ -107,6 +160,14 @@ export default function CocktailDetailsPage({
             </h3>
             {cocktailDetails && renderIngredients(cocktailDetails)}
           </div>
+          {/* Render Food Pairings */}
+          {renderFoodPairings()}
+          <Link href={'/search'}>
+            <button type="button" className="btn btn-primary">
+              <i className="fas fa-glass-cheers mr-2"></i>
+              Back to Search{' '}
+            </button>
+          </Link>
         </div>
       </div>
     </div>
